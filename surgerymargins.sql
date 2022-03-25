8378ec48d8b943da468c43d0f33de29bd27759e8
trice979 <treyrice79@gmail.com>
2022-03-25 16:24:13 -0500

    Update surgerymargins.sql

--- surgerymargins.sql
+++ surgerymargins.sql
@@ -3,49 +3,49 @@ DECLARE @endDate DATE = '2021-12-31';
 
 WITH			ORSpecialty AS (		
 SELECT			sc.HospitalAccount ,	
-                sc.Prov_ID_Legacy ,	
-				sum(cast(sc.ORTIME as float)) ORTime ,	
-				ROW_NUMBER() OVER(PARTITION BY sc.HospitalAccount ORDER BY sc.HospitalAccount , sum(cast(sc.ORTIME as float)) DESC) as Row#	
+                	sc.Prov_ID_Legacy ,	
+			sum(cast(sc.ORTIME as float)) ORTime ,	
+			ROW_NUMBER() OVER(PARTITION BY sc.HospitalAccount ORDER BY sc.HospitalAccount , sum(cast(sc.ORTIME as float)) DESC) as Row#	
 FROM			DSS..SurgCases sc	
 group by		sc.HospitalAccount ,			
-				sc.Prov_ID_Legacy)	
+			sc.Prov_ID_Legacy)	
 
-,				PrimSpec AS (	
+,			PrimSpec AS (	
 SELECT			*		
 FROM			ORSpecialty		
 WHERE			ORSpecialty.Row# = 1)	
 
 SELECT			enc.EncounterID ,	
-				enc.PatientType	,
-				enc.AdmissionDate ,	
-				enc.DischargeDate ,	
-				dt.year_fiscal ,
-                FORMAT(enc.DischargeDate, 'yyyy-MM') DischargeMonth ,	
-				enc.TotalCharges ,	
-				ISNULL(enc.TotalActualPayment, 0) TotalActualPayment, 
-                enc.VariableDirectCost ,	
-				enc.FixedDirectCost ,	
-				enc.VariableIndirectCost ,	
-				enc.FixedIndirectCost ,	
-				ISNULL(enc.TotalActualPayment, 0) - enc.TotalDirectCosts DirectMargin ,	
-				ISNULL(enc.TotalActualPayment, 0) - enc.TotalVariableCosts Contribution ,	
-				ISNULL(enc.TotalActualPayment, 0) - enc.TotalCosts NetIncome ,
-				enc.ARBalance ,
-				udf.Description FinancialClass ,	
-				ps.Prov_ID_Legacy ,
-                ph.last_name + ', ' + ph.first_name SurgeonName ,
-                ph.Division AcademicDept ,
-                ph.Specialty Division
+			enc.PatientType	,
+			enc.AdmissionDate ,	
+			enc.DischargeDate ,	
+			dt.year_fiscal ,
+                	FORMAT(enc.DischargeDate, 'yyyy-MM') DischargeMonth ,	
+			enc.TotalCharges ,	
+			ISNULL(enc.TotalActualPayment, 0) TotalActualPayment, 
+                	enc.VariableDirectCost ,	
+			enc.FixedDirectCost ,	
+			enc.VariableIndirectCost ,	
+			enc.FixedIndirectCost ,	
+			ISNULL(enc.TotalActualPayment, 0) - enc.TotalDirectCosts DirectMargin ,	
+			ISNULL(enc.TotalActualPayment, 0) - enc.TotalVariableCosts Contribution ,	
+			ISNULL(enc.TotalActualPayment, 0) - enc.TotalCosts NetIncome ,
+			enc.ARBalance ,
+			udf.Description FinancialClass ,	
+			ps.Prov_ID_Legacy ,
+                	ph.last_name + ', ' + ph.first_name SurgeonName ,
+                	ph.Division AcademicDept ,
+                	ph.Specialty Division
 					
 FROM			T_IP_ENCOUNTER enc		
-INNER JOIN      DSS..DATE dt
-                    on enc.DischargeDate = dt.date
+INNER JOIN      	DSS..DATE dt
+                    		on enc.DischargeDate = dt.date
 INNER JOIN		PrimSpec ps			
-					ON enc.PatientAccount = ps.HospitalAccount
+				ON enc.PatientAccount = ps.HospitalAccount
 LEFT JOIN		T_UDF_DESCRIPTION_8 udf			
-					on enc.UserField2 = udf.Code
-LEFT JOIN       DSS..PHYSICIAN ph
-                    on ps.Prov_ID_Legacy = ph.physician_number
+				on enc.UserField2 = udf.Code
+LEFT JOIN       	DSS..PHYSICIAN ph
+                    		on ps.Prov_ID_Legacy = ph.physician_number
 
 WHERE			enc.DischargeDate BETWEEN @startDate AND @endDate	
 
